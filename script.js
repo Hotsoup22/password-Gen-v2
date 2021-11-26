cdadf69ee0df798f286e39a6b1ad780e04ba403a
hotsoup42 <jose22gw@gmail.com>
2021-11-26 14:27:33 -0800

    added bootstrap libary, started script.js with function generatePassword

---
+++ script.js
@@ -0,0 +1,9 @@
+// when generate password btn is clicked 
+const generatePasswordBtn = document.querySelector("#generate-password-btn")
+generatePasswordBtn.addEventListener("click", generatePassword)
+
+function generatePassword(){
+  return  console.log("generatePassword Function, console.log")
+}
+//then it will check which boxes are checked 
+//then password will apear in textBox
