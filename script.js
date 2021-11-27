5c1878cf9aaf6814d59b3bbda6c3d5dfb126eba8
hotsoup42 <jose22gw@gmail.com>
2021-11-26 20:46:03 -0800

    created function to ischeck uppercase and a consoleLog box status check

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
+
+
+const lowercaseBox = document.querySelector("#lowercaseBox")
+const specialLetterBox = document.querySelector("#specialLetterBox")
+const numbersBox = document.querySelector("#numbersBox")
+const passwordLength = document.querySelector("#passwordLength")
+
+
+
+
 // when generate password btn is clicked 
-const generatePasswordBtn = document.querySelector("#generate-password-btn")
+const generatePasswordBtn = document.querySelector("#generatePasswordBtn")
 generatePasswordBtn.addEventListener("click", generatePassword)
 
-function generatePassword(){
-  return  console.log("generatePassword Function, console.log")
+function generatePassword() { 
+  checkUppercaseBox();
+  console.log("end of gen-pass function");
+}
+
+function checkUppercaseBox() {
+  const uppercaseBox = document.querySelector("#uppercaseBox")
+  let isCheckedUppercaseBox = false;
+  if (!uppercaseBox.checked === true) {
+    isCheckedUppercaseBox =false; 
+    return  consoleLogBoxCheck(isCheckedUppercaseBox);
+  } else if(uppercaseBox.checked === true) {
+    isCheckedUppercaseBox = true;
+  return consoleLogBoxCheck(isCheckedUppercaseBox );
+  }
+}
+
+function consoleLogBoxCheck(isChecked){
+  return  console.log("uppercasebox: "+ `${isChecked}`)
 }
 //then it will check which boxes are checked 
 //then password will apear in textBox
