1ded2ab6e9d567fc373ac4e7bba3ce99cf70cd40
hotsoup42 <jose22gw@gmail.com>
2021-11-27 13:40:15 -0800

    created function randomizePassword, consoleLogBoxCheck

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
-
+const upperCaseIndex = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const lowerCaseIndex = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const specialLettersIndex = ["$^?:,(){}[]~-_.@%+\'!#"]
+const numberIndex = ["1234567890"]
+let password = [];
 const lowercaseBox = document.querySelector("#lowercaseBox")
 const specialLetterBox = document.querySelector("#specialLetterBox")
 const numbersBox = document.querySelector("#numbersBox")
@@ -14,9 +18,18 @@ generatePasswordBtn.addEventListener("click", generatePassword)
 
 function generatePassword() { 
   checkUppercaseBox();
+
+ 
   console.log("end of gen-pass function");
 }
 
+function randomizePassword(myArray){
+  var rand = Math.floor(Math.random()*myArray.length);
+  var rValue = myArray[rand]
+ return console.log(rValue)
+}
+
+//then it will check which boxes are checked 
 function checkUppercaseBox() {
   const uppercaseBox = document.querySelector("#uppercaseBox")
   let isCheckedUppercaseBox = false;
@@ -25,12 +38,14 @@ function checkUppercaseBox() {
     return  consoleLogBoxCheck(isCheckedUppercaseBox);
   } else if(uppercaseBox.checked === true) {
     isCheckedUppercaseBox = true;
-  return consoleLogBoxCheck(isCheckedUppercaseBox );
+  return randomizePassword(upperCaseIndex) , consoleLogBoxCheck(isCheckedUppercaseBox );
   }
 }
 
-function consoleLogBoxCheck(isChecked){
-  return  console.log("uppercasebox: "+ `${isChecked}`)
+function consoleLogBoxCheck (isChecked){
+
+  return console.log("UpperCase value: ",`${isChecked}`)
 }
-//then it will check which boxes are checked 
 //then password will apear in textBox
+
+//then display values user chose 
