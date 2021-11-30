bdfd83b191ff3822b6d4f6d1fefabf8be1168cd6
hotsoup42 <jose22gw@gmail.com>
2021-11-30 14:29:47 -0800

    created a checknumbers()

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 const upperCaseIndex = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 const lowerCaseIndex = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-const specialLettersIndex = ["$^?:,(){}[]~-_.@%+\'!#"]
-const numberIndex = ["1234567890"]
+const specialLettersIndex = ["$","^","?",":",",","(",")","{","}","[","]","~","-","_",".","@","%","+","\",","!","#",",","'"]
+const numberIndex = ["1","2","3","4","5","6","7","8","9","0"]
 let password = [];
 
-const specialLetterBox = document.querySelector("#specialLetterBox")
-const numbersBox = document.querySelector("#numbersBox")
+
+
 const passwordLength = document.querySelector("#passwordLength")
 //can it fit in a function? scope problems?
 const passwordTextArea = document.querySelector("#passwordTextArea")
@@ -23,6 +23,8 @@ function generatePassword() {
   password = [];
   checkUppercaseBox();
   checkLowercaseBox();
+  checkSpecialLetterBox();
+  checkNumbersBox();
   DisplayPassword();
 
  
@@ -43,10 +45,10 @@ function checkUppercaseBox() {
   let isCheckedUppercaseBox = false;
   if (!uppercaseBox.checked === true) {
     isCheckedUppercaseBox =false; 
-    return  consoleLogBoxCheck(isCheckedUppercaseBox);
+    return  consoleLogBoxCheck(isCheckedUppercaseBox," :Uppercase");
   } else if(uppercaseBox.checked === true) {
     isCheckedUppercaseBox = true;
-  return randomizePassword(upperCaseIndex) , consoleLogBoxCheck(isCheckedUppercaseBox );
+  return randomizePassword(upperCaseIndex) , consoleLogBoxCheck(isCheckedUppercaseBox, " :Uppercase" );
   }
 }
 function checkLowercaseBox() {
@@ -54,18 +56,42 @@ function checkLowercaseBox() {
   let isCheckedLowercaseBox = false;
   if (!lowercaseBox.checked === true) {
     isCheckedLowercaseBox =false; 
-      consoleLogBoxCheck(isCheckedLowercaseBox);
+      consoleLogBoxCheck(isCheckedLowercaseBox, " :Lowercase");
   } else if(lowercaseBox.checked === true) {
     isCheckedLowercaseBox = true;
    randomizePassword(lowerCaseIndex) 
-    consoleLogBoxCheck(isCheckedLowercaseBox);
+    consoleLogBoxCheck(isCheckedLowercaseBox, " :Lowercase");
+  }
+}
+function checkSpecialLetterBox() {
+  const specialLetterBox = document.querySelector("#specialLetterBox")
+  let isCheckedSpecialLetterBox = false;
+  if (!specialLetterBox.checked === true) {
+    isCheckedSpecialLetterBox =false; 
+      consoleLogBoxCheck(isCheckedSpecialLetterBox, " :SpecialLetters");
+  } else if(specialLetterBox.checked === true) {
+    isCheckedSpecialLetterBox = true;
+   randomizePassword(specialLettersIndex) 
+    consoleLogBoxCheck(isCheckedSpecialLetterBox, " :SpecialLetters");
+  }
+}
+function checkNumbersBox() {
+  const numbersBox = document.querySelector("#numbersBox")
+  let isCheckedNumbersBox = false;
+  if (!numbersBox.checked === true) {
+    isCheckedNumbersBox =false; 
+      consoleLogBoxCheck(isCheckedNumbersBox, " :numbersBox");
+  } else if(numbersBox.checked === true) {
+    isCheckedNumbersBox = true;
+   randomizePassword(numberIndex) 
+    consoleLogBoxCheck(isCheckedNumbersBox, " :numbersBox");
   }
 }
 
-function consoleLogBoxCheck (isCheckedUppercaseBox, isCheckedLowercaseBox){
+function consoleLogBoxCheck (isCheckedUppercaseBox, b){
  
-   console.log("UpperCase value: ",`${isCheckedUppercaseBox}`)
-   console.log("LowerCase value: ",`${isCheckedLowercaseBox}`)
+   console.log(`${isCheckedUppercaseBox }`+b)
+   
 }
 //then password will apear in textBox
 function DisplayPassword(){
