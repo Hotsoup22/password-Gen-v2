45db3d79be334269c1165e3d7fe2d7deec8c0eb1
hotsoup42 <jose22gw@gmail.com>
2021-12-20 20:55:41 -0800

    added navbar template  from react-bootstrap

--- client/password-generator/src/components/Header/Header.js
+++ client/password-generator/src/components/Header/Header.js
@@ -1,11 +1,12 @@
-import Navbar from "../Nav/Nav";
+import NavbarComponent from "../Nav/Nav";
 
 export default function Header(){
 
     return(
         <header>
-            <h1>its a header</h1>
-            <Navbar/>
+            <h1>Welcome</h1>
+       
+            <NavbarComponent/>
         </header>
     )
 }
--- client/password-generator/src/components/Nav/Nav.js
+++ client/password-generator/src/components/Nav/Nav.js
@@ -1,7 +1,33 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
 
-export default function Navbar(){
-
-    return(
-        <nav>its a Navbar!</nav>
-    )
-} 
+export default function NavbarComponent(){
+return (
+  // <nav>hi</nav>
+ <Navbar bg="light" expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+     <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action2">Link</Nav.Link>
+        <Nav.Link href="#" disabled>
+          Link
+        </Nav.Link>
+      </Nav>
+      <Navbar.Collapse className="justify-content-end">
+      <Navbar.Text>
+        Signed in as: <a href="#login">Mark Otto</a>
+      </Navbar.Text>
+    </Navbar.Collapse>
+    </Navbar.Collapse> 
+   </Container>
+ </Navbar>  
+ )
+ };
