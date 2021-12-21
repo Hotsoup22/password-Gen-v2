70e15c79364cd43100918f1f29c50f9740a6f93f
hotsoup42 <jose22gw@gmail.com>
2021-12-20 20:42:21 -0800

    added footer import from react-ui-ki

--- client/password-generator/src/components/Footer/Footer.js
+++ client/password-generator/src/components/Footer/Footer.js
@@ -1,9 +1,13 @@
-
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from 'mdb-react-ui-kit';
 export default function Footer(){
     
     return (
    
-        <footer> its a Footer!</footer>
+        <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} MIT 
+        </MDBContainer>
+      </div>
         
     )
 }
