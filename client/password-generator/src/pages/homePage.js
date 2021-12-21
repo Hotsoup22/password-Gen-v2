298971798ef353629bdc7c4067008373ff255e22
hotsoup42 <jose22gw@gmail.com>
2021-12-21 14:15:15 -0800

    fixed .css import

--- client/password-generator/src/pages/homePage.js
+++ client/password-generator/src/pages/homePage.js
@@ -1,7 +1,7 @@
 import PasswordGen from "../components/Password-Gen/password-gen.js";
 
 import Header from '../components/Header/Header'
-import homepageStyle from "./assets/homepageStyle.css";
+import "./assets/homepageStyle.css";
 import Footer from '../components/Footer/Footer'
 export default function HomePage(){
  
