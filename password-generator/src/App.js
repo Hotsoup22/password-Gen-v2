ec366c89af473d426e80adf3025714419040044d
Hotsoup22 <jose22gw@gmail.com>
2022-03-10 20:14:55 -0800

    deleted comments and commented out import logo

--- password-generator/src/App.js
+++ password-generator/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import HomePage from './pages/homePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
@@ -7,23 +7,6 @@ import './App.css';
 function App() {
   return (
     <HomePage/>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
