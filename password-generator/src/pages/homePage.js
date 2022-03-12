8de5c230dee357a15e1b1f39705aed0e1ceb1429
Hotsoup22 <jose22gw@gmail.com>
2022-03-11 17:08:16 -0800

    commented out import header, and {header}in line 11

--- password-generator/src/pages/homePage.js
+++ password-generator/src/pages/homePage.js
@@ -1,6 +1,6 @@
 import PasswordGen from "../components/password-gen/password-gen";
 
-import Header from '../components/Header/Header'
+// import Header from '../components/Header/Header'
 import "./assets/homepageStyle.css";
 import Footer from '../components/Footer/Footer'
 export default function HomePage(){
@@ -8,11 +8,10 @@ export default function HomePage(){
 
       return(
         <div className="container">
-        <Header/>
+        {/* <Header/> */}
         <PasswordGen/>
         <Footer/>
         </div>
 
       )
-    // }
 }
